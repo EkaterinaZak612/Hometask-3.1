69c8bdc8030ccd7e63b5825f7331ed9778822bb6
Ekaterina Zakharova <e.zakharova.astafieva@gmail.com>
2023-04-03 14:28:07 +0200

    gitignore

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
     public int calculate(int price) {
-        int miles =price/20;
+        int miles = price / 20;
         if (price < 20) {
-            miles = 1 ;
+            miles = 1;
         }
         return miles;
     }
